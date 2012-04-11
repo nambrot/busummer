9cf5f4b7c1f81b6bb267a350ca918f2fd598d08b
Connor McEwen <connor.mcewen@gmail.com>
2012-04-10 23:11:05 -0400

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Busummer::Application.config.secret_token = '94eb48061f9f3f924096d3e4a931edc15a0f193a821c915526fe437ad61e97b2d2505bfe662e46d0438768c38535a3312d399546b7f0435fd68b371d56f0fef0'
